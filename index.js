06aba21ba08b8817d56cc483c6c35c9ed7412ec7
Hush <allannelly690@gmail.com>
2023-12-10 00:29:41 +0300

    assignment 1: write code and unit tests for job scheduling problem

---
+++ index.js
@@ -0,0 +1,113 @@
+const fs = require('fs');
+
+
+/**
+ * Represents a job with a start time, end time, and profit.
+ * @constructor
+ * @param {string} startTime - The start time of the job in HHMM format.
+ * @param {string} endTime - The end time of the job in HHMM format.
+ * @param {string} profit - The profit associated with completing the job.
+ */
+function Job(startTime, endTime, profit) {
+    this.startTime = Number(startTime);
+    this.endTime = Number(endTime);
+    this.profit = Number(profit);
+}
+
+
+/**
+ * Finds the last job that does not conflict with the given job.
+ * @param {Job[]} jobs - Array of jobs.
+ * @param {number} index - Index of the job to check against.
+ * @returns {number} - Index of the last non-conflicting job or -1 if none found.
+ */
+function findLastNonConflict(jobs, index) {
+    for (let i = index - 1; i >= 0; i--) {
+        if (jobs[i].endTime <= jobs[index].startTime) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+/**
+ * Determines the jobs to be taken to maximize profit without overlapping.
+ * @param {Job[]} jobs - Array of jobs.
+ * @returns {Job[]} - Array of jobs chosen to maximize profit.
+ */
+function determineJobsTaken(jobs) {
+    let n = jobs.length;
+    let chosenJobs = [];
+    let i = n - 1;
+
+    while (i >= 0) {
+        let lastNonConflict = findLastNonConflict(jobs, i);
+        if (lastNonConflict === -1 || jobs[i].profit + (
+            lastNonConflict === -1 ? 0 : jobs[lastNonConflict].profit) > jobs[i - 1].profit
+            ) {
+            chosenJobs.push(jobs[i]);
+            i = lastNonConflict;
+        } else {
+            i--;
+        }
+    }
+
+    return chosenJobs.reverse();
+}
+
+/**
+ * Computes the number of jobs and total earnings left for other employees.
+ * @param {Job[]} jobs - Array of all available jobs.
+ * @returns {number[]} - Array containing the number of jobs left and the total earnings left.
+ */
+function jobsLeftForOthers(jobs) {
+    let totalEarnings = jobs.reduce((acc, job) => acc + job.profit, 0);
+    let jobsForJohn = determineJobsTaken(jobs);
+    let earningsForJohn = jobsForJohn.reduce((acc, job) => acc + job.profit, 0);
+    const result = [jobs.length - jobsForJohn.length, totalEarnings - earningsForJohn];
+    writeOutputToFile('jobs.output.txt', result);
+    return result;
+}
+
+
+/**
+ * Reads the content of a file.
+ * @param {string} filePath - Path to the file to be read.
+ * @returns {string} - Content of the file.
+ */
+function readFile(filePath) {
+    return fs.readFileSync(filePath, 'utf8');
+}
+
+
+/**
+ * Reads a file and processes it to determine jobs left for others.
+ * @param {string} filePath - Path to the file containing job data.
+ * @returns {number[]} - Array containing the number of jobs left and the total earnings left.
+ */
+function readAndProcessFile(filePath) {
+    const data = readFile(filePath);
+    const lines = data.trim().split('\n');
+    let jobs = [];
+
+    for (let i = 2; i <= lines.length - 1; i += 3) {
+        jobs.push(new Job(lines[i], lines[i + 1], lines[i + 2]));
+    }
+
+    return jobsLeftForOthers(jobs);
+}
+
+
+/**
+ * Writes the given data to a file.
+ * @param {string} filePath - Path to the file where data will be written.
+ * @param {number[]} data - The data to be written to the file.
+ */
+function writeOutputToFile(filePath, data) {
+    const formattedData = `Jobs left: ${data[0]}, Earnings left: ${data[1]}`;
+    fs.writeFileSync(filePath, formattedData, 'utf8');
+}
+
+module.exports = {
+    Job, readAndProcessFile, jobsLeftForOthers, readFile
+}
---
+++ index.test.js
@@ -0,0 +1,60 @@
+const { Job, jobsLeftForOthers, readFile, readAndProcessFile } = require('./index');
+
+jest.mock('fs');
+
+const fs = require('fs');
+
+describe('Job Scheduler Tests', () => {
+    it('Job constructor should create a job object correctly', () => {
+        const job = new Job('0900', '1030', '100');
+        expect(job).toEqual({ startTime: 900, endTime: 1030, profit: 100 });
+    });
+
+    test.each([
+        // [jobs, expected result]
+        [
+            [
+                new Job('0900', '1030', '100'),
+                new Job('1000', '1200', '500'),
+                new Job('1100', '1200', '300')
+            ],
+            [2, 400]
+        ],
+        [
+            [
+                new Job('0900', '1000', '250'),
+                new Job('0945', '1200', '550'),
+                new Job('1130', '1500', '150')
+            ],
+            [2, 400]
+        ],
+        [
+            [
+                new Job('0900', '1030', '100'),
+                new Job('1000', '1200', '100'),
+                new Job('1100', '1200', '100')
+            ],
+            [1, 100]
+        ]
+    ])('jobsLeftForOthers should return correct result for given jobs', (jobs, expectedResult) => {
+        expect(jobsLeftForOthers(jobs)).toEqual(expectedResult);
+    });
+
+    it('readFile should correctly read data from file', () => {
+        const mockData = '1\n3\n0900\n1030\n100\n1000\n1200\n500\n1100\n1200\n300';
+        fs.readFileSync.mockReturnValue(mockData);
+
+        const filePath = 'test.txt';
+        const data = readFile(filePath);
+
+        expect(data).toBe(mockData);
+        expect(fs.readFileSync).toHaveBeenCalledWith(filePath, 'utf8');
+    });
+
+    it('readAndProcessFile should correctly parse raw data into jobs and calculate result', () => {
+        const rawData = '1\n3\n0900\n1030\n100\n1000\n1200\n500\n1100\n1200\n300';
+        const result = readAndProcessFile(rawData);
+        expect(result).toEqual([2, 400]);
+    });
+});
+
